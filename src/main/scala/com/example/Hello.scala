480b78ea19144387c494a4e595aa8c955abdfad8
shigemk2 <i.am.shige@gmail.com>
2016-03-14 21:17:29 +0900

    Add Lense first

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,12 @@
 package com.example
 
+import monocle.Lens
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    case class Example(s: String, i: Int)
+    val s = Lens[Example, String](_.s)(s => _.copy(s = s))
+    println(s)
     println("Hello, world!")
   }
 }
