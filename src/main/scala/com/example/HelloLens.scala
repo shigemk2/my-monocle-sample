ee239ceef7c20cb696bf540fab64913ab8c58a61
shigemk2 <i.am.shige@gmail.com>
2016-03-14 21:21:13 +0900

    Rename Hello to HelloLens

--- src/main/scala/com/example/HelloLens.scala
+++ src/main/scala/com/example/HelloLens.scala
@@ -2,7 +2,7 @@ package com.example
 
 import monocle.Lens
 
-object Hello {
+object HelloLens {
   def main(args: Array[String]): Unit = {
     case class Example(s: String, i: Int)
     val s = Lens[Example, String](_.s)(s => _.copy(s = s))
