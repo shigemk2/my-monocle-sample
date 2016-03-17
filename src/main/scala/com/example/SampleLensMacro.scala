4b971e96d277b0a11c60e4c2712f85c9b277201b
shigemk2 <i.am.shige@gmail.com>
2016-03-17 12:20:49 +0900

    マクロ なんか出来てない

---
+++ src/main/scala/com/example/SampleLensMacro.scala
@@ -0,0 +1,33 @@
+package com.example
+
+import monocle.macros.Lenser
+
+
+object SampleLensMacro {
+  def main(args: Array[String]): Unit = {
+    // Streetのnameを不変性を保ったまま書き換えたい
+    case class Street(name: String)
+    case class Address(street: Street)
+    case class Company(address: Address)
+    case class Employee(company: Company)
+
+    val lenser = Lenser[Employee]
+
+    val _name = lenser(_.name)
+    val _street = lenser(_.street)
+    val _address = lenser(_.address)
+    val _company = lenser(_.company)
+
+    // 不変ッッ
+    val employee = Employee(Company(Address(Street("chuodori"))))
+    println((_company composeLens _address composeLens _street composeLens _name) get employee)
+    val employeeHoge = (_company composeLens _address composeLens _street composeLens _name).set("hogedori")(employee)
+    println((_company composeLens _address composeLens _street composeLens _name) get employeeHoge)
+    val employeeCap = (_company composeLens _address composeLens _street composeLens _name).modify(_.capitalize)(employee)
+    println((_company composeLens _address composeLens _street composeLens _name) get employeeCap)
+    println((_company ^|-> _address ^|-> _street ^|-> _name).modify(_.capitalize)(employee))
+
+
+    println("Hello, world!")
+  }
+}
