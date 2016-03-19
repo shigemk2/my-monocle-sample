e69d574f758301019c6d0bc54de49b11fecdce47
shigemk2 <i.am.shige@gmail.com>
2016-03-19 15:03:35 +0900

    Fix SamplePrism

--- src/main/scala/com/example/SamplePrismCaseClass.scala
+++ src/main/scala/com/example/SamplePrismCaseClass.scala
@@ -15,17 +15,17 @@ object SamplePrismCaseClass {
     case class Sample(day: Day)
 
     val _tuesday = Prism[Sample, Day]{
-      case x: Day => Option(x)
+      case x if x.day == Tuesday() => Option(x.day)
       case _       => None
     } { x =>
       Sample(x)
     }
 
-    println(_tuesday.reverseGet(Tuesday()))
+    println(_tuesday.reverseGet(Tuesday())) // Sample(Tuesday())
     println(_tuesday.getOption(Sample(Monday()))) // None
-    println(_tuesday.getOption(Sample(Tuesday()))) // Some(())
+    println(_tuesday.getOption(Sample(Tuesday()))) // Some(Tuesday())
     println(_tuesday.getMaybe(Sample(Monday()))) // Empty()
-    println(_tuesday.getMaybe(Sample(Tuesday()))) // Just(())
+    println(_tuesday.getMaybe(Sample(Tuesday()))) // Just(Tuesday())
     println(Sample(Tuesday()).day)
     println("Hello, world!")
   }
